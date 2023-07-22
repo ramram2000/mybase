c87c0723dad83743609ef896da7fe5c9246d87e4
ramram2000 <83423439+ramram2000@users.noreply.github.com>
2023-07-22 22:43:26 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xbcac71f34fd98e6c4a9361919554175bd7d1d05d";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
